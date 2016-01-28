2a4c3e3d3e235d55d461a1ed0b6bba428ce4bb05
Abigail Watson <awatson1978@gmail.com>
2016-01-27 21:00:58 -0800

    updated package.js

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
-  api.use('clinical:router');
+  api.use('clinical:router@2.0.17');
   api.addFiles('router-default-config.js');
 });
 
